5329e619279df6c7929273d883f658ce12caa2d7
MoonSat <chiragnikolic@gmail.com>
2025-11-15 11:02:26 -0500

    fix: solidity version in token.sol

--- src/WMon.sol
+++ src/WMon.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
